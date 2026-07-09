b1e9cbdcee95464256532d8b2ed4b72946b3d444
Muhammad Inam <nudmaufon@gmail.com>
2026-07-09 21:21:02 +0500

    Enhance microservice configuration and functionality
    - Updated .gitignore to include additional files and directories.
    - Modified docker-compose.yaml to load environment variables from .env file.
    - Added Kubernetes ConfigMap and Secret for application configuration and database credentials.
    - Created services and deployments for user and greeting services with updated configurations.
    - Implemented user creation and retrieval endpoints in the user service.
    - Added database connection handling in the user service.

--- user_service/main.py
+++ user_service/main.py
@@ -1,15 +1,16 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
 import datetime
 import httpx
-
-GREETING_URI='http://greeting_service:8000'
+from database import USER, get_db
+GREETING_URI='http://greeting-service:80'
 
 
 app=FastAPI()
 
 
 @app.get("/")
-async def get_user():
+async def get_user(db :Session = Depends(get_db)):
 
     async with httpx.AsyncClient() as client:
         try:
@@ -23,12 +24,41 @@ async def get_user():
 
         except Exception as e:
             greeting_message = f"Error calling greeting service: {str(e)}"
+        
+    first_user=db.query(USER).first()
     return {
         "service": "user-service",
         "message": "User data fetched successfully",
         "greeting_from_other_service": greeting_message,
+        "first_user_in_db": {
+            "id": first_user.id,
+            "name": first_user.name,
+            "email": first_user.email
+        } if first_user else None,
         "status": "healthy",
         "timestamp": datetime.datetime.now().isoformat()
+    }
+
+@app.post("/users/")
+async def user_create(name:str , email:str,db: Session = Depends(get_db)):
+    # Check if user already exists
+    db_user = db.query(USER).filter(USER.email == email).first()
+    if db_user:
+        raise HTTPException(status_code=400, detail="Email already registered")
+    
+    # New user create karna
+    new_user = USER(name=name, email=email)
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    
+    return {
+        "message": "User created successfully",
+        "user": {
+            "id": new_user.id,
+            "name": new_user.name,
+            "email": new_user.email
+        }
     }
 
 
